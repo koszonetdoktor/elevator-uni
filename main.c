0ce052be9fedaa1dd0c6e99d4200f2767e7092d8
koszonetdoktor <halmi.gergo@gmail.com>
2015-02-17 11:49:51 +0100

    Main.c adding

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * main.c
+ */
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/debug.h"
+#include "driverlib/fpu.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/pwm.h"
+#include "driverlib/rom.h"
+#include "driverlib/rom_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/timer.h"
+#include "driverlib/uart.h"
+
+int main(void) {
+	
+	return 0;
+}
