2631613d33bf3814df368fe59b556347b8175bd5
koszonetdoktor <halmi.gergo@gmail.com>
2015-02-27 19:35:10 +0100

    Updated main.c
    Put in FloorLearn, bt not working yet. Modified the Inits.

--- main.c
+++ main.c
@@ -22,21 +22,26 @@
 #include "uartfunc.h"
 #include "uartstdio.h"
 
+//void FloorLearn(void);
+int value;
 int g_ui32SysClock;
 
-unsigned char floor_switch;
+//volatile unsigned char floor_switch;
+
 
 int main(void) {
 	g_ui32SysClock = MAP_SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ |
 		                                     SYSCTL_OSC_MAIN | SYSCTL_USE_PLL |
 		                                     SYSCTL_CFG_VCO_480), 120000000);
-
-	PWMInit();  // initialized the pwm signals used by the motors
-	LeftEncoderInit();
-	RigthEncoderInit();
+	SensorsInit();  //initialize all the sensors
+	PWMInit0();
+	PWMInit1();
 	UARTInit();
-	UARTprintf("k");
 
+
+	SlowMotion(LEFT_LIFT,DOWNWARD);
+
+	//FloorLearn();
 	//PWM test
 	/*
 	PWMPulseWidthSet(PWM0_BASE, PWM_OUT_0 , 20000); // 1 Volt
@@ -45,6 +50,8 @@ int main(void) {
 	PWMPulseWidthSet(PWM0_BASE, PWM_OUT_3 , 50000); // 2,5 Volt
 	*/
 	while(1){
+		UARTprintf("%d\n",counter_left);
+		SysCtlDelay(2000);
 
 	}
 	
@@ -55,38 +62,47 @@ int main(void) {
 // Learning Cycel
 //**********************************************************************
 void FloorLearn(void){
-	unsigned char floor_switch=0;
 	unsigned char floor_counter = 0;
 	int floor_distance[10];
-	unsigned char learn_switch = 0;
 
 	while(1){
 		switch(floor_switch){
-		case START_LEFT:	SlowMotion(LEFT_LIFT,DOWNWARD); // slow motion to the left lift down
+		case START_LEFT:	UARTprintf("Slow motion...Down\n");
+							SlowMotion(LEFT_LIFT,DOWNWARD);
+							floor_switch = WAIT_STOP;
+							break;
+
+		case WAIT_STOP:		UARTprintf("wait\n");
 							if(GPIOPinRead(GPIO_PORTB_BASE,GPIO_PIN_0) && GPIOPinRead(GPIO_PORTB_BASE,GPIO_PIN_1)){ // ha a reed és a photo szintérzékelő érzékel
-								floor_switch = CYCLE_LEFT;
+								floor_switch = END_LEFT;
+								UARTprintf("switch to the END_LEFT\n");
 							}
 							break;
 
-		case CYCLE_LEFT:	SlowMotion(LEFT_LIFT, UPWARD);
+		case CYCLE_LEFT:	//UARTprintf("slow motion up..\n");
+							SlowMotion(LEFT_LIFT, UPWARD);
 							counter_left = 0;
-							floor_switch = LEARNING_LEFT;
+							floor_switch = WAIT_LEFT;
 							break;
 
-		case LEARNING_LEFT:	if(GPIOPinRead(GPIO_PORTB_BASE,GPIO_PIN_1)){  // nézi a bal oldali lift szintérzékelőjét
+		case LEARNING_LEFT:	UARTprintf("Counting the floors and the distances....\n");
+							if(GPIOPinRead(GPIO_PORTB_BASE,GPIO_PIN_1)){  // nézi a bal oldali lift szintérzékelőjét
 								floor_distance[floor_counter] = counter_left; // elmenti az értéket
 								floor_counter++; // tovább lép a következő szintre
 								floor_switch = WAIT_LEFT;
 							}
 							break;
 
-		case WAIT_LEFT:		if(!(GPIOPinRead(GPIO_PORTB_BASE,GPIO_PIN_1))){ // vár arra hogy elhagyja a szint jeladót
+		case WAIT_LEFT:		UARTprintf("wait to leave the led..\n");
+							if(!(GPIOPinRead(GPIO_PORTB_BASE,GPIO_PIN_1))){ // vár arra hogy elhagyja a szint jeladót
 								counter_left = 0;
+								swi2 = 1;
 								floor_switch = LEARNING_LEFT;
 							}
 							break;
 
-		case END_LEFT: 		StopMotion(LEFT_LIFT);  // eza fügvény még nincs megírva!
+		case END_LEFT: 		UARTprintf("end, so stop!\n");
+							StopMotion(LEFT_LIFT);
 							floor_switch = START_RIGHT;
 							break;
 
@@ -97,3 +113,4 @@ void FloorLearn(void){
 
 	}
 }// end of FloorLearn
+
