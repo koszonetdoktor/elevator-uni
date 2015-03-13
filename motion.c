852d69c908464f51a0f781e830a674ce56550e75
koszonetdoktor <halmi.gergo@gmail.com>
2015-03-13 16:39:48 +0100

    Update motion.c
    Move function with velocity put in.

--- motion.c
+++ motion.c
@@ -22,7 +22,11 @@
 #include "driverlib/sysctl.h"
 #include "driverlib/timer.h"
 #include "driverlib/uart.h"
+#include "math.h"
+#include "timers.h"
+#include "motion.h"
 
+int desired_v;
 
 
 void PWMInit(void){ //intitalized all the pwms..(PF0,PF1,PF2,PF3) NEM HASZNÁLOM!!
@@ -138,46 +142,90 @@ void PWMInit3(void){
 }
 
 void SlowMotion(unsigned char lift,unsigned char direction){		// lift=1(right),lift=0(left)  direction=0(up),direction=1(down)
-	if(lift == 0){
-		if(direction == 0){
-			PWMPulseWidthSet(PWM0_BASE, PWM_OUT_0 , 1);
+	if(lift == 0){ // left
+		if(direction == 0){ // up
+			PWMPulseWidthSet(PWM0_BASE, PWM_OUT_0 ,1 );
 			SysCtlDelay(200);
-			PWMPulseWidthSet(PWM0_BASE, PWM_OUT_1 , 15000);
+			PWMPulseWidthSet(PWM0_BASE, PWM_OUT_1 , 12090); // v= 30 upward
 		}
 		if(direction == 1){
 			PWMPulseWidthSet(PWM0_BASE, PWM_OUT_1 , 1);
 			SysCtlDelay(200);
-			PWMPulseWidthSet(PWM0_BASE, PWM_OUT_0 , 15000);
+			PWMPulseWidthSet(PWM0_BASE, PWM_OUT_0 , 10000); // v = 30 Downward
 		}
 		else{
 			return -1;
 		}
 	}
 	if(lift == 1){
-		if(direction == 0){
-			PWMPulseWidthSet(PWM0_BASE, PWM_OUT_2 , 1);
-			SysCtlDelay(200);
-			PWMPulseWidthSet(PWM0_BASE, PWM_OUT_3 , 10000);
-		}
-		if(direction == 1){
-			PWMPulseWidthSet(PWM0_BASE, PWM_OUT_2 , 10000);
+		if(direction == 0){ // up
+			PWMPulseWidthSet(PWM0_BASE, PWM_OUT_2 , 15508); // v =~ 330
 			SysCtlDelay(200);
 			PWMPulseWidthSet(PWM0_BASE, PWM_OUT_3 , 1);
 		}
+		if(direction == 1){
+			PWMPulseWidthSet(PWM0_BASE, PWM_OUT_2 , 1);
+			SysCtlDelay(200);
+			PWMPulseWidthSet(PWM0_BASE, PWM_OUT_3 , 12000); // v =~ 330
+		}
 		else{
 			return -1;
 		}
 	}
 
 
-}// end of SlowMotion()
+}// end of SlowMotion(
+
+void Move(unsigned char lift,unsigned char direction, float velocity){
+	// azt még lehetne ezen fejleszteni, hogy legyen egy univerzális sebesség, mert így mind a kettő más más tartományban mozog
+
+	if((velocity>60) && (lift == 0)){ // this is for safety,
+		velocity = 60;
+	}
+	if((velocity>700) && (lift == 1)){
+		velocity = 700;
+	}
+
+	if(lift == 0){  // left
+		left_desired_v = velocity;
+		left_dir = direction;
+		if(direction == 0){  // up
+			PWMPulseWidthSet(PWM0_BASE, PWM_OUT_0 ,1 );
+			SysCtlDelay(200);
+			PWMPulseWidthSet(PWM0_BASE, PWM_OUT_1 , (10000 * (velocity/30)));
+		}
+		if(direction == 1){ // down
+			PWMPulseWidthSet(PWM0_BASE, PWM_OUT_1 , 1);
+			SysCtlDelay(200);
+			PWMPulseWidthSet(PWM0_BASE, PWM_OUT_0 , (10000 * (velocity/30)));
+		}
+	}
+	if(lift == 1){  // right
+		right_desired_v = velocity;
+		right_dir = direction;
+		if(direction == 0){ // up
+			PWMPulseWidthSet(PWM0_BASE, PWM_OUT_2 , (15508 * (velocity/330)));
+			SysCtlDelay(200);
+			PWMPulseWidthSet(PWM0_BASE, PWM_OUT_3 , 1);
+		}
+		if(direction == 1){ // down
+			PWMPulseWidthSet(PWM0_BASE, PWM_OUT_2 , 1);
+			SysCtlDelay(200);
+			PWMPulseWidthSet(PWM0_BASE, PWM_OUT_3 , (12000 * (velocity/330)));
+		}
+	}
+} // end of move
 
 void StopMotion(unsigned char lift){
-	if(lift == 1){
+	if(lift == 1){ // right
+		right_desired_v = 0;
+		right_dir = NODIR;
 		PWMPulseWidthSet(PWM0_BASE, PWM_OUT_2 , 1);
 		PWMPulseWidthSet(PWM0_BASE, PWM_OUT_3 , 1);
 	}
-	if(lift == 0){
+	if(lift == 0){ // left
+		left_desired_v = 0;
+		left_dir = NODIR;
 		PWMPulseWidthSet(PWM0_BASE, PWM_OUT_0 , 1);
 		PWMPulseWidthSet(PWM0_BASE, PWM_OUT_1 , 1);
 	}
@@ -185,4 +233,25 @@ void StopMotion(unsigned char lift){
 		return -1;
 	}
 }
+
+
+/****************************************************************
+ * Under Development
+ */
+void Acceleration(){
+
+
+}
+
+
+int Deceleration(int distance){
+	unsigned char a = 2;
+	int velocity;
+
+	velocity = sqrt(2*a*distance); // this is the main equation of the deceleration
+
+	return velocity;
+}
+
+
 
