3b6d29e5a5f9308fdf9f318495cd3d8c730a2157
koszonetdoktor <halmi.gergo@gmail.com>
2015-03-13 16:45:17 +0100

    Update uartfunc.h
    New init and Int Handler for the UART1.

--- uartfunc.h
+++ uartfunc.h
@@ -8,7 +8,11 @@
 #ifndef UART_H_
 #define UART_H_
 
+extern char uart1_message;
+
 extern void UARTInit(void);
 extern void UARTSend(const uint8_t *pui8Buffer, uint32_t ui32Count);
+extern void UART1Init(void);
+void UART1IntHandler(void);
 
 #endif /* UART_H_ */
